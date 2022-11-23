9fedcf8318f937706bf6942a9238dbb1681c7928
ArturYalik <lyo3a6rata@gmail.com>
2022-11-23 20:22:37 +0200

    Update TestTable.java

--- src/test/java/TestTable.java
+++ src/test/java/TestTable.java
@@ -19,40 +19,33 @@ public class TestTable {
         wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
     }
 
-//    @Test
-//    public void tableTestCSS() {
-//        WebElement canada = wd.findElement(By.cssSelector("#customers tr:nth-child(8) td:last-child"));
-//        String text = canada.getText();
-//        System.out.println(text);
-//        Assert.assertEquals(text, "Canada");
-//    }
-//
-//    @Test
-//    public void tableHW(){
-//        WebElement row4 = wd.findElement(By.cssSelector("#customers tr:nth-child(4)"));
-//        System.out.println(row4.getText());
-//        String text = row4.getText();
-//        Assert.assertTrue(text.contains("Mexico"));
-//    }
+
+    @Test
+    public void tableHW(){
+        WebElement row4 = wd.findElement(By.cssSelector("#customers tr:nth-child(4)"));
+        System.out.println(row4.getText());
+        String text = row4.getText();
+        Assert.assertTrue(text.contains("Mexico"));
+    }
 
     @Test
     public void tableHW2(){
-//        List<WebElement> rows = wd.findElements(By.cssSelector("#customers tr"));
-//        System.out.println(rows.size());
-//        Assert.assertEquals(rows.size(),9);
-//
-//        List<WebElement> cols = wd.findElements(By.cssSelector("#customers th"));
-//        System.out.println(cols.size());
-//        Assert.assertEquals(cols.size(),3);
-//
-//        WebElement row3 = wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
-//        System.out.println(row3.getText());
+        List<WebElement> rows = wd.findElements(By.cssSelector("#customers tr"));
+        System.out.println(rows.size());
+        Assert.assertEquals(rows.size(),9);
 
-//        WebElement lastCol = wd.findElement(By.cssSelector("#customers tr:last-child td:last-child"));
-//        System.out.println(lastCol.getText());
+        List<WebElement> cols = wd.findElements(By.cssSelector("#customers th"));
+        System.out.println(cols.size());
+        Assert.assertEquals(cols.size(),3);
 
-        WebElement rowName = wd.findElement(By.cssSelector("#customers tr:nth-child(7) td));
-        System.out.println();
+        WebElement row3 = wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
+        System.out.println(row3.getText());
+
+        WebElement lastCol = wd.findElement(By.cssSelector("#customers tr:last-child td:last-child"));
+        System.out.println(lastCol.getText());
+
+//        List<WebElement> rowName = wd.findElements(By.cssSelector("#customers tr:nth-child(7) td"));
+//        System.out.println(rowName);
         // print number of row with name "Philip Cramer"
     }
 
